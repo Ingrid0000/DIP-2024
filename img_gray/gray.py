3f452896c2c479ffe51e9552e09e8722fc4818df
Ingrid0000 <114061678+Ingrid0000@users.noreply.github.com>
2024-10-28 22:49:25 +0800

    Update gray.py

--- img_gray/gray.py
+++ img_gray/gray.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('image.jpg')
+img = cv2.imread('/home/vboxuser/DIP/00009_TE_1976x1312.png')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imwrite('output.jpg', img_gray)
