9d3fc6437c444945bc2b663b996e0e9f9d10c508
Ingrid0000 <114061678+Ingrid0000@users.noreply.github.com>
2024-10-28 22:46:55 +0800

    Update psnr1.py

--- PSNR/psnr1.py
+++ PSNR/psnr1.py
@@ -2,8 +2,8 @@ import cv2
 from skimage.metrics import mean_squared_error
 from skimage.metrics import peak_signal_noise_ratio
 
-img1 = cv2.imread('/home/vboxuser/DIP/B&W.png')
-img2 = cv2.imread('/home/vboxuser/DIP/Compressed.png')
+img1 = cv2.imread('/home/vboxuser/DIP/B&W_09.png')
+img2 = cv2.imread('/home/vboxuser/DIP/Compressed_LeGall53_09.jp2')
 
 MSE = mean_squared_error(img1, img2)
 PSNR = peak_signal_noise_ratio(img1, img2)
