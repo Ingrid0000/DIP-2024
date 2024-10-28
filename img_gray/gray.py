df796941c4dbad97a5e2556a0a205116f6f09046
Ingrid0000 <114061678+Ingrid0000@users.noreply.github.com>
2024-10-28 20:29:41 +0800

    Update gray.py

--- img_gray/gray.py
+++ img_gray/gray.py
@@ -1,12 +1,9 @@
 import cv2
 
 img = cv2.imread('image.jpg')
-
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imwrite('output.jpg', img_gray)
-
 cv2.imshow('Image', img_gray)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
