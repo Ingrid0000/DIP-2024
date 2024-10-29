257bec81573873239c0b0cf236de8ed62ce31a87
Ingrid0000 <114061678+Ingrid0000@users.noreply.github.com>
2024-10-30 04:05:34 +0800

    Create demo.py

---
+++ Haar/demo.py
@@ -0,0 +1,37 @@
+'''
+Demo showing forward and inverse Haar wavelet decomposition.
+The input can be in color or grayscale.
+
+Please note that in order to save as an image, a clipping operation
+is performed on the Haar wavelets, which discards negative values.
+'''
+
+import cv2
+import numpy as np
+from timeit import default_timer as timer
+from PIL import Image
+from haar_wavelets import haar_forward, haar_inverse
+
+
+
+def main():
+
+    ref = Image.open("/home/vboxuser/DIP/B&W_06.png")
+    ref = np.asarray(ref).astype(np.float32)
+
+    LEVELS = 3
+    start = timer()
+    hwt = haar_forward(ref.copy(), LEVELS) # forward transform
+    inv = haar_inverse(hwt.copy(), LEVELS)  # backward transform
+    end = timer()
+
+    Image.fromarray(hwt.clip(0,255).astype(np.uint8)).save("06_wavelets_for.png") # save visualization
+    cv2.imwrite('06_wavelets_for.jp2', hwt.clip(0,255).astype(np.uint8))
+    Image.fromarray(inv.clip(0,255).astype(np.uint8)).save("06_wavelets_inv.png") # save reconstructed image
+    cv2.imwrite('06_wavelets_inv.jp2', inv.clip(0,255).astype(np.uint8))
+    
+    print("Time taken in seconds for forward and inverse Haar wavelet transform:", end-start)
+
+    
+if __name__ == "__main__":
+    main()
