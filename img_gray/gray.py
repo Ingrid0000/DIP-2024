07849ab4a1f9619c3fb5468b8babe1fec6e57a85
Ingrid0000 <114061678+Ingrid0000@users.noreply.github.com>
2025-01-03 01:05:09 +0800

    Update gray.py

--- img_gray/gray.py
+++ img_gray/gray.py
@@ -1,9 +1,10 @@
 import cv2
 
-img = cv2.imread('/home/vboxuser/DIP/00009_TE_1976x1312.png')
+img = cv2.imread('/home/vboxuser/DIP/00001_TE_2096x1400.png')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite('B&W_09.png', img_gray)
+cv2.imwrite('B&W_01.png', img_gray)
 cv2.imshow('Image', img_gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
